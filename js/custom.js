110475ab956811f2cf1462bd015e5359f768924a
James-Fan <jf2865@nyu.edu>
2018-01-02 23:43:22 -0500

    nav bar upgrade
    created a split overlay menu

--- js/custom.js
+++ js/custom.js
@@ -10,3 +10,63 @@ $('.count').each(function () {
     });
 });
 
+
+
+
+/*function myFunction(x) {
+    x.classList.toggle("change");
+} */
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+
+// Close the dropdown if the user clicks outside of it
+/* window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+} */
+
+function openNav() {
+    document.getElementById("myNav").style.height = "100%";
+    document.getElementById("myNav").style.zIndex = "-100";
+    document.getElementById("menu").setAttribute( "onClick", "javascript: closeNav();" );
+    document.getElementById("logo").style.zIndex = "10000";
+
+    document.getElementById("myNav2").style.height = "100%";
+    document.getElementById("myNav2").style.zIndex = "-100";
+
+    document.getElementById("menu").classList.toggle("change");
+
+
+
+
+
+
+
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.height = "0%";
+    document.getElementById("myNav").style.zIndex = "-100";
+    document.getElementById("menu").setAttribute( "onClick", "javascript: openNav();" );
+    document.getElementById("logo").style.zIndex = "10000";
+
+    document.getElementById("myNav2").style.height = "0%";
+    document.getElementById("myNav2").style.zIndex = "-100";
+
+    document.getElementById("menu").classList.toggle("change");
+
+
+
+
+}
+
