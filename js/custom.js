356d47019d38fb5014669eb02499b889e3b019e5
James-Fan <jf2865@nyu.edu>
2018-01-08 04:30:24 -0500

    Added donate buttons and qr
    r and donation buttons

--- js/custom.js
+++ js/custom.js
@@ -12,6 +12,8 @@ $('.count').each(function () {
 
 
 
+        
+
 
 /*function myFunction(x) {
     x.classList.toggle("change");
@@ -90,4 +92,26 @@ function openTab(evt, tabName) {
 document.getElementById("defaultOpen").click();
 
 
+/* Ethereum Tip Button */
+/*
+var tipButton = document.querySelector('.tip-button')
+tipButton.addEventListener('click', function() {
+  if (typeof web3 === 'undefined') {
+    return renderMessage('You need to install MetaMask to use this feature.  https://metamask.io')
+  }
+
+  var user_address = web3.eth.accounts[0]
+  web3.eth.sendTransaction({
+    to: YOUR_ADDRESS,
+    from: user_address,
+    value: web3.toWei('1', 'ether'),
+  }, function (err, transactionHash) {
+    if (err) return renderMessage('Oh no!: ' + err.message)
+
+    // If you get a transactionHash, you can assume it was sent,
+    // or if you want to guarantee it was received, you can poll
+    // for that transaction to be mined first.
+    renderMessage('Thanks!')
+  })
+}) */
 
